fa395d17c5e155c232e6d228b63e790b15a67d56
YouJaeMin <wovkek@naver.com>
2016-10-04 10:08:04 +0900

    start

---
+++ mybatisdemo/src/part01/EmpTest.java
@@ -0,0 +1,9 @@
+package part01;
+
+public class EmpTest {
+
+	public static void main(String[] args) {
+
+	}
+	
+}
---
+++ mybatisdemo/src/part01/EmployeeDAO.java
@@ -0,0 +1,44 @@
+package part01;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import part01.dto.EmployeeDTO;
+
+public class EmployeeDAO {
+	SqlSession session;
+
+	public EmployeeDAO() {
+		// 1. 트렌젝션이 설정되어 있는 파일의 경로를 정의 한다.
+		String resource = "configuration.xml";
+
+		try {
+			// 2. 설정파일을 로딩하기 위해서 입력스트림과 연결한다.
+			Reader reader = Resources.getResourceAsReader(resource);
+			// 3. 설정파일을 저장하기 위한 팩토리를 생성해줄수 있는 빌더를 생성한다.
+			SqlSessionFactoryBuilder build = new SqlSessionFactoryBuilder();
+			// 4. 설정파일의 정보를 읽어와 관리해주는 팩토리를 생성한다.
+			SqlSessionFactory factory = build.build(reader);
+			// 5. 쿼리문에 접근할 수 있도록 sqlSession객체를 리턴한다.
+			// openSession() 의 기본값은 false이다.
+			// 자동커밋을 설정하고 싶으면 openSession(true)로 설정한다.
+			session = factory.openSession(true);
+
+			// 6.입력스트림 연결을 종료한다.
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	} // end
+
+	public List<EmployeeDTO> allMethod() {
+		return session.selectList("emp.all");
+	}
+
+}
